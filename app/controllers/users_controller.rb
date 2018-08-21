3940bb9466d8a3221e7ee765f7477e88e3acad60
Kenny <kennybrown11@hotmail.com>
2018-08-21 21:02:18 +0000

    added feature user-specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   private
     def user_params
         params.require(:user).permit(
-            :name,
+            :full_name,
             :email,
             :password,
             :password_confirmation
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
-class User < ActiveRecord::Base
 
+class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
+  
+  validates :name, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :password, :length => {:within => 6..40}
+  validates :password_confirmation, :length => {:within => 6..40}
+
 
 end
