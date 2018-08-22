36d18a8d0985b4259f3acaf2712a10ecab7b0d37
Kenny <kennybrown11@hotmail.com>
2018-08-22 01:09:16 +0000

    added product details spec

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+     10.times do |n|
+      @category.products.create!(
+    name:  Faker::Hipster.sentence(3),
+    description: Faker::Hipster.paragraph(4),
+    image: open_asset('apparel1.jpg'),
+    quantity: 10,
+    price: 64.99
+    )
+    end
+  end
+
+  scenario "they click on product to view details" do
+    visit root_path
+
+    first('article.product').click_link 'Details'
+
+    expect(page).to have_css 'article.product-detail'
+
+  end
+end
